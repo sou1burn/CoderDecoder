0d0b6bec32aaa4cd4100193b1b508c259de4aaba
sou1burn <koryakinv_04@mail.ru>
2025-03-13 16:30:52 +0300

    add step-by-step syndrome counting

--- src/decoder/decoder.cpp
+++ src/decoder/decoder.cpp
@@ -1,5 +1,5 @@
 #include "decoder.h"
-
+//dop C
 namespace coder {
     void Decoder::setDecoderData(std::vector<byte> &poly, const byte &k, std::vector<byte> &codeWord, std::vector<byte> &errorVector)
     {
@@ -20,14 +20,39 @@ namespace coder {
 
         m_syndrome.resize(m_b.size(), 0);
 
-        for (auto i = 0; i <= m_b.size() - m_polynom.size(); ++i) {
+        std::cout << "\nb(x): ";
+        for (auto byte : m_b) std::cout << static_cast<int>(byte);
+        std::cout << "\n";
+
+        std::cout << "g(x): ";
+        for (auto byte : m_polynom) std::cout << static_cast<int>(byte);
+        std::cout << "\n\n";
+
+        for (size_t i = 0; i <= m_b.size() - m_polynom.size(); ++i) {
             if (m_b[i] == 1) {
-                for (auto j = 0; j < m_polynom.size(); ++j) {
-                    m_syndrome[i + j] ^= m_polynom[j];
+                std::cout << "Step " << i << ":\n";
+
+                std::cout << "m_b before step: ";
+                for (auto byte : m_b) std::cout << static_cast<int>(byte);
+                std::cout << "\n";
+                std::cout << "g(x): ";
+                for (auto byte : m_polynom) std::cout << static_cast<int>(byte);
+                std::cout << "\n";
+
+                for (size_t j = 0; j < m_polynom.size(); ++j) {
+                    m_b[i + j] ^= m_polynom[j];
+                    m_syndrome[i + j] = m_b[i + j];
                 }
+
+                std::cout << "m_b after step:  ";
+                for (auto byte : m_b) std::cout << static_cast<int>(byte);
+                std::cout << "\n";
             }
         }
-
+        // g(x):1110
+        // n:8
+        // error vector:10110111
+        // m:11101
         for (const auto pow : m_syndrome)
             if (pow != 0)
                 return false;
