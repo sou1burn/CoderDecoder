2838712e46c579269264b4d3070b46678cf20ad0
sou1burn <koryakinv_04@mail.ru>
2025-04-09 00:01:30 +0300

    [*] fix output

--- main.cpp
+++ main.cpp
@@ -1,53 +1,61 @@
 #include "decoder.h"
 
-std::vector<coder::byte> parseBinaryString(const std::string& binaryStr)
-{
-    std::vector<coder::byte> result;
-    for (char bit : binaryStr) {
-        if (bit == '0' || bit == '1')
-            result.push_back(bit - '0');
-        else
-           throw std::runtime_error("Error: Only binary input allowed");
+namespace helpers {
+    std::vector<coder::byte> parseBinaryString(const std::string& binaryStr)
+    {
+        std::vector<coder::byte> result;
+        for (char bit : binaryStr) {
+            if (bit == '0' || bit == '1')
+                result.push_back(bit - '0');
+            else
+                throw std::runtime_error("Error: Only binary input allowed");
 
+        }
+
+        return result;
     }
 
-    return result;
-}
-
-std::vector<std::vector<coder::byte>> generateOddErrorVector(const int &len)
-{
-    std::vector<std::vector<coder::byte>> oddErrorVectors;
-    const size_t numVectors = 1 << len;
-    for (size_t i = 1; i < numVectors; ++i) {
-        std::vector<coder::byte> errorVector(len, 0);
-        int count = 0;
-        for (size_t j = 0; j < len; ++j) {
-            if (i & (1 << j)) {
-                errorVector[j] = 1;
-                ++count;
+    std::vector<std::vector<coder::byte>> generateOddErrorVector(const int &len)
+    {
+        std::vector<std::vector<coder::byte>> oddErrorVectors;
+        const size_t numVectors = 1 << len;
+        for (size_t i = 1; i < numVectors; ++i) {
+            std::vector<coder::byte> errorVector(len, 0);
+            int count = 0;
+            for (size_t j = 0; j < len; ++j) {
+                if (i & (1 << j)) {
+                    errorVector[j] = 1;
+                    ++count;
+                }
             }
+            if (count % 2 == 1)
+                oddErrorVectors.push_back(errorVector);
         }
-        if (count % 2 == 1)
-            oddErrorVectors.push_back(errorVector);
+        return oddErrorVectors;
     }
-    return oddErrorVectors;
-}
 
-std::vector<std::vector<coder::byte>> generateMessages(const int &len)
-{
-    std::vector<std::vector<coder::byte>> messages;
-    const size_t numVectors = 1 << len;
-    for (auto i = 0; i < numVectors; ++i) {
-        std::vector<coder::byte> msg(len, 0);
-        for (auto j = 0; j < len; ++j) {
-            if (i & (1 << j))
-                msg[j] = 1;
+    std::vector<std::vector<coder::byte>> generateMessages(const int &len)
+    {
+        std::vector<std::vector<coder::byte>> messages;
+        const size_t numVectors = 1 << len;
+        for (auto i = 0; i < numVectors; ++i) {
+            std::vector<coder::byte> msg(len, 0);
+            for (auto j = 0; j < len; ++j) {
+                if (i & (1 << j))
+                    msg[j] = 1;
+            }
+            messages.push_back(msg);
         }
-        messages.push_back(msg);
+        return messages;
     }
-    return messages;
-}
 
+    std::string printByteVector(const std::vector<coder::byte>& vec) {
+        std::string result;
+        for (const auto &byte : vec)
+            result += std::to_string(static_cast<int>(byte));
+        return result;
+    }
+}
 int main() {
     std::string polynom, n, errVec, sequence;
     //, option;
@@ -61,9 +69,9 @@ int main() {
     std::cout << "Enter m: ";
     std::cin >> sequence;
 
-    auto polynomVec = parseBinaryString(polynom);
-    auto errorVector = parseBinaryString(errVec);
-    auto msgVector = parseBinaryString(sequence);
+    auto polynomVec = helpers::parseBinaryString(polynom);
+    auto errorVector = helpers::parseBinaryString(errVec);
+    auto msgVector = helpers::parseBinaryString(sequence);
     const coder::byte nValue = std::stoi(n);
 
     coder::Coder coder;
@@ -111,18 +119,19 @@ int main() {
     std::cout << "Пусть порождающий многочлен равен fi(x) * (x+1) = (x^3 + x + 1) * (x + 1) = x^4 + x^3 + x^2 + 1, а n было введено ранее" << std::endl;
     std::cout << "Генерируем 2^(n -r) сообщений и 2^n / 2 векторов ошибок" << std::endl;
     gx = {1, 1, 1, 0, 1}; // g(x) = x^4 + x^3 + x^2 + 1
+    auto count = 0;
     coder::Coder dopCoder;
     coder::Decoder dopDecoder;
-    auto oddErrorVectors = generateOddErrorVector(nValue);
+    auto oddErrorVectors = helpers::generateOddErrorVector(nValue);
     std::vector<std::pair<std::vector<coder::byte>, std::vector<coder::byte>>> messagesAndErrorVectors;
 
-    auto messages = generateMessages(nValue - (gx.size() - 1));
+    auto messages = helpers::generateMessages(nValue - (gx.size() - 1));
 
     for (auto msg : messages) {
-        std::cout << "Сообщение сейчас:" << std::endl;
-        for (auto byte : msg)
-            std::cout << static_cast<int>(byte);
-        std::cout << std::endl;
+        // std::cout << "Сообщение сейчас:" << std::endl;
+        // for (auto byte : msg)
+        //     std::cout << static_cast<int>(byte);
+        // std::cout << std::endl;
 
         dopCoder.setCoderData(gx, nValue, msg);
         auto codeWordDop = dopCoder.codeWord();
@@ -137,41 +146,37 @@ int main() {
             bool dopResult = dopDecoder.makeDecision();
 
             if (dopResult) {
-                std::cout << "Ошибка не обнаружена для вектора: ";
-                for (auto byte : errorVec)
-                    std::cout << static_cast<int>(byte);
-                std::cout << std::endl;
+                // std::cout << "Ошибка не обнаружена для вектора: ";
+                // for (auto byte : errorVec)
+                //     std::cout << static_cast<int>(byte);
+                // std::cout << std::endl;
                 messagesAndErrorVectors.push_back({msg, errorVec});
-            }
+            } else
+                count++;
         }
     }
 
     std::cout << "Статистика для g(x) = x^4 + x^3 + x^2 + 1"<< std::endl;
-    std::cout << "Всего найдено ошибок:" << messagesAndErrorVectors.size() << std::endl;
+    std::cout << "Всего не найдено ошибок:" << messagesAndErrorVectors.size() << std::endl;
+    std::cout << "Всего найдено ошибок:" << count << std::endl;
     std::cout << "Пары сообщение - вектор ошибок, для которых не были обнаружены ошибки: " << std::endl;
-    std::cout << "Сообщение m: ";
 
-    for (const auto &pair : messagesAndErrorVectors) {
-        for (auto byte : pair.first)
-            std::cout << static_cast<int>(byte);
-        std::cout << std::endl;
-    }
-    std::cout << "Вектор ошибок e: ";
-    for (const auto &pair : messagesAndErrorVectors) {
-        for (auto byte : pair.second)
-            std::cout << static_cast<int>(byte);
-        std::cout << std::endl;
-    }
+    for (const auto &pair : messagesAndErrorVectors)
+        std::cout << "Сообщение m: " << helpers::printByteVector(pair.first) << " Вектор ошибок e: " << helpers::printByteVector(pair.second) << std::endl;
+    std::cout << "\n----------------------------------------------------------------------\n"<< std::endl;
+    std::cout << "\n________________________________________________________________________\n"<< std::endl;
 
     std::cout << "Пусть порождающий многочлен равен fi(x) * (x+1) = (x^3 + x^2 + 1) * (x + 1) = x^4 + x^2 + x + 1, а n было введено ранее"<< std::endl;
     gx = {1, 0, 1, 1, 1}; // g(x) = x^4 + x^2 + x + 1
     messagesAndErrorVectors.clear();
 
+    count = 0;
+
     for (auto message : messages) {
-        std::cout << "Сообщение сейчас:" << std::endl;
-        for (auto byte : message)
-            std::cout << static_cast<int>(byte);
-        std::cout << std::endl;
+        // std::cout << "Сообщение сейчас:" << std::endl;
+        // for (auto byte : message)
+        //     std::cout << static_cast<int>(byte);
+        // std::cout << std::endl;
 
         dopCoder.setCoderData(gx, nValue, message);
         auto codeWordDop1 = dopCoder.codeWord();
@@ -186,31 +191,24 @@ int main() {
             bool dopResult = dopDecoder.makeDecision();
 
             if (dopResult) {
-                std::cout << "Ошибка не обнаружена для вектора: ";
-                for (auto byte : errorVec)
-                    std::cout << static_cast<int>(byte);
-                std::cout << std::endl;
+                // std::cout << "Ошибка не обнаружена для вектора: ";
+                // for (auto byte : errorVec)
+                //     std::cout << static_cast<int>(byte);
+                // std::cout << std::endl;
                 messagesAndErrorVectors.push_back({message, errorVec});
-            }
+            } else
+                count++;
         }
     }
 
     std::cout << "Статистика для g(x) = x^4 + x^2 + x + 1"<< std::endl;
-    std::cout << "Всего найдено ошибок:" << messagesAndErrorVectors.size() << std::endl;
+    std::cout << "Всего не найдено ошибок:" << messagesAndErrorVectors.size() << std::endl;
+    std::cout << "Всего найдено ошибок:" << count << std::endl;
     std::cout << "Пары сообщение - вектор ошибок, для которых не были обнаружены ошибки: " << std::endl;
-    std::cout << "Сообщение m: ";
 
-    for (const auto &pair : messagesAndErrorVectors) {
-        for (auto byte : pair.first)
-            std::cout << static_cast<int>(byte);
-        std::cout << std::endl;
-    }
-    std::cout << "Вектор ошибок e: ";
-    for (const auto &pair : messagesAndErrorVectors) {
-        for (auto byte : pair.second)
-            std::cout << static_cast<int>(byte);
-        std::cout << std::endl;
-    }
+    for (const auto &pair : messagesAndErrorVectors)
+        std::cout << "Сообщение m: " << helpers::printByteVector(pair.first) << " Вектор ошибок e: " << helpers::printByteVector(pair.second) << std::endl;
+
     return 0;
 
 }
